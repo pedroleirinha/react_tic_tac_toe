53cb813a2ea96a2b1a6f707b329b66c982f42f98
Thunder <oleirinha.pedro@gmail.com>
2020-11-30 19:15:32 +0000

    startup code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,105 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+function Square(props) {
+	let index = 0;
+	return (
+		<button
+			className="square"
+			id={`button-${index}`}
+			onClick={props.onClick}
+		>
+			{props.value}
+		</button>)
+}
+
+class Board extends React.Component {
+
+	renderSquare(row, column) {
+		return (
+			<Square
+				value={this.props.squares[row][column]}
+				onClick={() => this.props.onClick(row, column)}
+			/>
+		)
+	}
+
+	render() {
+		return (
+			<div className="board">
+				<div className="row">
+					{this.renderSquare(0, 0)}
+					{this.renderSquare(0, 1)}
+					{this.renderSquare(0, 2)}
+				</div>
+				<div className="row">
+					{this.renderSquare(1, 0)}
+					{this.renderSquare(1, 1)}
+					{this.renderSquare(1, 2)}
+				</div>
+				<div className="row">
+					{this.renderSquare(2, 0)}
+					{this.renderSquare(2, 1)}
+					{this.renderSquare(2, 2)}
+				</div>
+			</div>
+		);
+	}
+}
+
+
+class Game extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			history: [{
+				squares: [
+					[null, null, null],
+					[null, null, null],
+					[null, null, null]
+				]
+			}],
+			currentPlayer: "X",
+			gameOver: false
+		}
+	}
+
+	render() {
+		const status = "";
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board
+						squares={this.state.history[0].squares}
+						onClick={() => { console.log("click") }}
+					/>
+				</div>
+
+				<div className="game-info">
+					<div>{status}</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+
+
+
+
+
+
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Game />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
